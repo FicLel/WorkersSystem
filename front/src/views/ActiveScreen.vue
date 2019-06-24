be5bda54f7839cb1794b28034b2271c6245370f3
victor.martinez <victor.martinez@dev.uneatlantico>
2019-06-24 14:24:02 +0200

    added bulma

---
+++ front/src/views/ActiveScreen.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<div>
+			
+		</div>
+		<div>
+			
+		</div>
+	</div>
+</template>
+<script>
+	export default{
+		name: 'activescreen',
+		data(){
+			return{
+				active:{
+
+				}
+			}
+		}
+	}
+</script>
