2544a7374d30a3a0b9959e45764840b520116e2d
victor.martinez <victor.martinez@dev.uneatlantico>
2019-06-26 13:20:37 +0200

    more things

--- front/src/router.js
+++ front/src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-
+import ActiveScreen from './views/ActiveScreen'
 Vue.use(Router)
 
 export default new Router({
